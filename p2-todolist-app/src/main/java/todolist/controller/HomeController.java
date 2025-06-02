61e4f2501faa972a2b66cbc35419b27654ad279b
raul-quesada <raul.quesada@goumh.umh.es>
2025-06-02 11:41:42 +0200

    Create About Controller, View and Test. version 1.0.1-snapshot

---
+++ p2-todolist-app/src/main/java/todolist/controller/HomeController.java
@@ -0,0 +1,13 @@
+package todolist.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/about")
+    public String about(Model model) {
+        return "about";
+    }
+}
