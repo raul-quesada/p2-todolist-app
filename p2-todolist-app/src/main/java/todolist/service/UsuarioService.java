cade7291acf6f9f4b32d69e65edfa0c7bdd38db0
raul-quesada <raul.quesada@goumh.umh.es>
2025-06-05 11:14:20 +0200

    doc/p2.md

--- p2-todolist-app/src/main/java/todolist/service/UsuarioService.java
+++ p2-todolist-app/src/main/java/todolist/service/UsuarioService.java
@@ -13,7 +13,6 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 @Service
 public class UsuarioService {
